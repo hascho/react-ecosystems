748bb53147d0fe3a78ff73ef8accad6f9de5efb9
hassan <hassan.chowdhury1@gmail.com>
2020-05-10 19:43:44 +0100

    Redux: add create and remove actions/reducers and connect components to redux store

--- src/todos/TodoList.jsx
+++ src/todos/TodoList.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import NewTodoForm from './NewTodoForm.jsx';
 import TodoListItem from './TodoListItem.jsx';
+import { removeTodo } from './actions';
 import './TodoList.css';
 
-const TodoList = ({ todos = [] }) => (
+const TodoList = ({
+  todos = [],
+  onRemovePressed,
+}) => (
   <div className="list-wrapper">
     <NewTodoForm />
-    {todos.map(todo => <TodoListItem todo={todo} />)}
+    {todos.map(todo => <TodoListItem key={todo.text} todo={todo} onRemovePressed={onRemovePressed} />)}
   </div>
 );
 
-export default TodoList;
+const mapStateToProps = state => ({
+  todos: state.todos,
+});
+
+const mapDispatchToProps = dispatch => ({
+  onRemovePressed: text => dispatch(removeTodo(text)),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList);
--- src/todos/TodoListItem.jsx
+++ src/todos/TodoListItem.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './TodoListItem.css'
 
-const TodoListItem = ({ todo }) => (
+const TodoListItem = ({
+  todo,
+  onRemovePressed,
+}) => (
   <div className="todo-item-container">
     <h3>{todo.text}</h3>
     <div className="buttons-container">
       <button className="completed-button">Mark as Completed</button>
-      <button className="remove-button">Remove</button>
+      <button
+        onClick={() => onRemovePressed(todo.text)}
+        className="remove-button"
+      >
+        Remove
+      </button>
     </div>
   </div>
 );
 
-export default TodoListItem;
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoListItem);
