e4ffcf8b917767f5f369052399ce92faaf99633f
hassan <hassan.chowdhury1@gmail.com>
2020-05-10 17:41:00 +0100

    Basic Project: create todos + rename .eslintrc

---
+++ src/todos/TodoList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './TodoList.css';
+
+const TodoList = ({ todos }) => (
+  <div className="list-wrapper">
+    {todos.map(todo => <TodoListItem todo={todo} />)}
+  </div>
+);
+
+export default TodoList;
