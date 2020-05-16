c8fd7a0766b694ff77d4a285c22543ffd5ce3136
hassan <hassan.chowdhury1@gmail.com>
2020-05-16 17:41:03 +0100

    Redux: add redux persistence between browser refresh + add redux flow for marking todo as completed

--- src/todos/TodoList.jsx
+++ src/todos/TodoList.jsx
@@ -2,16 +2,25 @@ import React from 'react';
 import { connect } from 'react-redux';
 import NewTodoForm from './NewTodoForm.jsx';
 import TodoListItem from './TodoListItem.jsx';
-import { removeTodo } from './actions';
+import {
+  removeTodo,
+  markTodoAsCompleted,
+} from './actions';
 import './TodoList.css';
 
 const TodoList = ({
   todos = [],
   onRemovePressed,
+  onCompletedPressed,
 }) => (
   <div className="list-wrapper">
     <NewTodoForm />
-    {todos.map((todo, i) => <TodoListItem key={i} todo={todo} onRemovePressed={onRemovePressed} />)}
+    {todos.map((todo, i) =>
+      <TodoListItem
+        key={i}
+        todo={todo}
+        onRemovePressed={onRemovePressed}
+        onCompletedPressed={onCompletedPressed} />)}
   </div>
 );
 
@@ -21,6 +30,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   onRemovePressed: text => dispatch(removeTodo(text)),
+  onCompletedPressed: text => dispatch(markTodoAsCompleted(text)),
 });
 
 export default connect(
